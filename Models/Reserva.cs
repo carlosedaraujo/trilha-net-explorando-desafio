851c8a73dba20ed21fc929790750b8d900cc0a9f
Carlos Araujo <carlosed.araujo@gmail.com>
2024-04-24 15:34:08 -0300

    Implementacao dos TODOs do desafio de projeto

--- Models/Reserva.cs
+++ Models/Reserva.cs
@@ -17,7 +17,9 @@ namespace DesafioProjetoHospedagem.Models
         {
             // TODO: Verificar se a capacidade é maior ou igual ao número de hóspedes sendo recebido
             // *IMPLEMENTE AQUI*
-            if (true)
+
+            //IMPLEMENTADO
+            if (hospedes.Count <= Suite.Capacidade)
             {
                 Hospedes = hospedes;
             }
@@ -25,6 +27,9 @@ namespace DesafioProjetoHospedagem.Models
             {
                 // TODO: Retornar uma exception caso a capacidade seja menor que o número de hóspedes recebido
                 // *IMPLEMENTE AQUI*
+
+                //IMPLEMENTADO
+                throw new InvalidOperationException("Capacidade da suíte menor que o número de hóspedes");
             }
         }
 
@@ -37,7 +42,10 @@ namespace DesafioProjetoHospedagem.Models
         {
             // TODO: Retorna a quantidade de hóspedes (propriedade Hospedes)
             // *IMPLEMENTE AQUI*
-            return 0;
+            
+            //IMPLEMENTADO
+            return Hospedes.Count;
+            
         }
 
         public decimal CalcularValorDiaria()
@@ -45,15 +53,20 @@ namespace DesafioProjetoHospedagem.Models
             // TODO: Retorna o valor da diária
             // Cálculo: DiasReservados X Suite.ValorDiaria
             // *IMPLEMENTE AQUI*
-            decimal valor = 0;
 
+            //IMPLEMENTADO
+            decimal valor = DiasReservados * Suite.ValorDiaria;
+           
+           
             // Regra: Caso os dias reservados forem maior ou igual a 10, conceder um desconto de 10%
             // *IMPLEMENTE AQUI*
-            if (true)
-            {
-                valor = 0;
-            }
 
+            //IMPLEMENTADO
+            if (DiasReservados >= 10)
+            {
+                return valor * 0.9m;
+            }
+            
             return valor;
         }
     }
